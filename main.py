fa06017d43e694dec2be9650356e40e33184b874
Vladislav Osin <osinvladislav@yandex.ru>
2022-05-27 16:17:11 +0300

    чуть обновил
    чуть обновил

---
+++ main.py
@@ -0,0 +1,20 @@
+import wx
+import eel
+
+eel.init('web')
+
+
+@eel.expose
+def pythonFunction(wildcard="*"):
+    app = wx.App(None)
+    style = wx.FD_OPEN | wx.FD_FILE_MUST_EXIST
+    dialog = wx.FileDialog(None, 'Open', wildcard=wildcard, style=style)
+    if dialog.ShowModal() == wx.ID_OK:
+        path = dialog.GetPath()
+    else:
+        path = None
+    dialog.Destroy()
+    return path
+
+
+eel.start('index.html', size=(1600, 900))
